bdee87098333a9c1fc19535b0ef004af8c384b3d
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2021-12-15 14:36:33 -0600

    heroku fix: changing everything to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
