14222411eb13c45506c9033547250724d5d1dfc1
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2021-12-15 14:24:44 -0600

    change server to Flask version

--- server.py
+++ server.py
@@ -1,43 +1,7 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, render_template
 
-hostName = "localhost"
-import os
-ON_HEROKU = os.environ.get('ON_HEROKU')
+app = Flask(__name__)
 
-if ON_HEROKU:
-    # get the heroku port
-    serverPort = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    serverPort = 3000
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        if self.path == '/':
-            self.path = '/index.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-    
-
-
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+@app.route('/')
+def index():
+    return render_template('index.html')
